0b0169dd12ce40973913d18519340ffdd480086e
Arek <57560812+wojcik96@users.noreply.github.com>
2020-07-06 21:19:26 +0200

    Update post.php

--- build/post.php
+++ build/post.php
@@ -4,7 +4,6 @@ $do = 'arko00@wp.pl';
 $email = $_POST['email']; 
 $wiadomosc = $_POST['message']; 
 $temat = $_POST['name']; 
-//w $header tworzymy nagłówek e-mail (dane o kodowaniu, gdzie ma być e-mail wysłany itp.
 $header = "From: $email \nContent-Type:".
             ' text/plain;charset="UTF-8"'.
             "\nContent-Transfer-Encoding: 8bit";
